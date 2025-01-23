973d1425c151216d47da99e20dd8da2ea4093dbe
Yonatan <granmocasin@gmail.com>
2025-01-23 15:26:30 -0500

    initial commit

---
+++ target/debug/deps/magpie-92ecd8fd340f990f.d
@@ -0,0 +1,5 @@
+/home/alberto/proyectos/magpie/target/debug/deps/libmagpie-92ecd8fd340f990f.rmeta: src/main.rs
+
+/home/alberto/proyectos/magpie/target/debug/deps/magpie-92ecd8fd340f990f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/magpie-b9e354ea25df4034.d
@@ -0,0 +1,5 @@
+/home/alberto/proyectos/magpie/target/debug/deps/libmagpie-b9e354ea25df4034.rmeta: src/main.rs
+
+/home/alberto/proyectos/magpie/target/debug/deps/magpie-b9e354ea25df4034.d: src/main.rs
+
+src/main.rs:
